c5726afd0ff0ea996894d8a7d9d91a8cf9a0d8a4
blastarray <welshmar@gmail.com>
2023-04-22 09:34:02 -0400

    add european option outline

---
+++ option_pricing/b_s_eur_options.py
@@ -0,0 +1,20 @@
+import math
+from scipy.stats import norm
+
+def dOne(a, b, c):
+    return
+
+def NdOne(a, b, c):
+    return
+
+def dTwo(a, b, c):
+    return
+
+def NdTwo(a, b, c):
+    return
+
+def call_option(a, b, c):
+    return
+
+def put_option(a, b, c):
+    return
